3e84bd9437e12e9ceb5682c37796ff818180b98c
Dagan Martinez <martinez.dagan@gmail.com>
2021-10-24 13:24:18 -0400

    WIP Handle interrupts

--- vectors.cc
+++ vectors.cc
@@ -1,9 +1,10 @@
 #include <cstdint>
 #include "panic.h"
+#include "io.h"
 
 enum class ExceptionType : uint32_t {
     FIQ = 0x00,
-    RESET,
+    RESET ,
     ILLEGAL_INSTRUCTION,
     SWI,
     PREFETCH,
@@ -11,6 +12,14 @@ enum class ExceptionType : uint32_t {
     IRQ
 };
 
+static void irq_handler() {
+    printf("Oh boy it's an IRQ!\n");
+}
+
 extern "C" void exception_handler(ExceptionType exception_type) {
-    panic("Hey hey it's an exception of type %x!", exception_type);
+    if (exception_type == ExceptionType::IRQ) {
+        irq_handler();
+    } else {
+        panic("Hey hey it's an exception of type %x!", exception_type);
+    }
 }
