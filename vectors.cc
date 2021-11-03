42c33d447c45c0230fefd2f63415e9f3a3471a5b
Dagan Martinez <martinez.dagan@gmail.com>
2021-11-03 19:59:08 -0400

    fixup! WIP Handle interrupts

--- vectors.cc
+++ vectors.cc
@@ -3,13 +3,13 @@
 #include "io.h"
 
 enum class ExceptionType : uint32_t {
-    FIQ = 0x00,
-    RESET ,
-    ILLEGAL_INSTRUCTION,
-    SWI,
-    PREFETCH,
-    DATA,
-    IRQ
+    FIQ = 0x1C,
+    RESET = 0x00,
+    ILLEGAL_INSTRUCTION = 0x04,
+    SWI = 0x08,
+    PREFETCH = 0x0C,
+    DATA = 0x10,
+    IRQ = 0x18
 };
 
 static void irq_handler() {
@@ -20,6 +20,6 @@ extern "C" void exception_handler(ExceptionType exception_type) {
     if (exception_type == ExceptionType::IRQ) {
         irq_handler();
     } else {
-        panic("Hey hey it's an exception of type %x!", exception_type);
+        panic("Hey hey it's an exception of type 0x%x!", exception_type);
     }
 }
